3fe6570bb4fda45468e587c41ebdc1b82dccf05a
Peter Kofler <peter.kofler@code-cop.org>
2023-05-24 20:12:14 +0200

    Remove second part of exercise, only keep Refactoring part.

--- Composition/Java/Composition/src/main/java/algorithm/composition/HighPassFilter.java
+++
@@ -1,9 +0,0 @@
-package algorithm.composition;
-/// <summary>
-/// Should filter out measurements with an X or Y that are less than or equal to 2
-/// You'll need to implement IMeasureFilter to do the job
-/// </summary>
-///
-public class HighPassFilter {
-
-}
